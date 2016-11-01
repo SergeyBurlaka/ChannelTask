a5f454a57abaa10f37ba35d336ffc2a094213b3c
bananaBoom <skifoner@mail.ru>
2016-11-01 10:27:35 +0200

    git init

---
+++ app/src/main/java/com/example/kostya/channeltask/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.kostya.channeltask.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.kostya.channeltask.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
